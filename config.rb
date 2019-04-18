1659cdb0c7ecd5ae3f799b7d0925551b2b41bbeb
Alexandru Popa <8577646+popa-alex@users.noreply.github.com>
2019-04-18 14:55:43 +0300

    uploaded build folder
    uploaded build folder

--- config.rb
+++
@@ -1,9 +0,0 @@
-preferred_syntax = :scss
-http_path = '/'
-css_dir = 'public/stylesheets'
-sass_dir = 'public/sass'
-images_dir = 'public/images'
-javascripts_dir = 'src/javascripts'
-relative_assets = true
-line_comments = true
-# output_style = :compressed
---
+++ precache-manifest.1cbb956e0caaa12367fa9bdbd2ef053d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2bfc92331dad1b7e14df",
+    "url": "/static/js/main.ec9fad32.chunk.js"
+  },
+  {
+    "revision": "7e1235fe4e8adc7a7ae8",
+    "url": "/static/js/2.bf267eb1.chunk.js"
+  },
+  {
+    "revision": "35a7659f2d8daa517f4e87c60cc14c72",
+    "url": "/index.html"
+  }
+];
