a5e2326fbe56b64b650380a4ff1f96bede311463
Alexandru Popa <8577646+popa-alex@users.noreply.github.com>
2019-04-18 14:23:09 +0300

    Updates

---
+++ precache-manifest.975590515db9a36451df3fef87a92edd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a1bf7daac9ddf6b48dae",
+    "url": "/static/js/main.719a85ef.chunk.js"
+  },
+  {
+    "revision": "7e1235fe4e8adc7a7ae8",
+    "url": "/static/js/2.bf267eb1.chunk.js"
+  },
+  {
+    "revision": "0d12ed000f905ba03c7ab619a9c70a1a",
+    "url": "/index.html"
+  }
+];
